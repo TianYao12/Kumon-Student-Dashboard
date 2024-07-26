dcea050a04a2ba51aed47a47b1714d7b38156090
Tian Yao <xtyao2015@gmail.com>
2024-07-26 14:39:36 -0400

    added basic layout for student table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,64 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+interface StudentData {
+  id: string;
+  name: string;
+  time_entered: number; // change this later probably
+  time_left: number
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [studentData, setStudentData] = useState<StudentData[]>([]);
+
+  useEffect(() => {
+    const fetchStudentData = async() => {
+      try {
+        const response = await fetch("/api/getStudents");
+        const data = await response.json();
+        setStudentData(data);
+      } catch(error) {
+          console.error(error);
+      }
+    };
+    fetchStudentData();
+  }, [])
 
   return (
     <>
       <div className='main-container'>
         <h1>Student Dashboard</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
+        <div className="grid-container">
+          <div className="grid-column-heading">
+            <h2 className="grid-column-heading-text">Student ID</h2>
+          </div>
+          <div className="grid-column-heading">
+            <h2 className="grid-column-heading-text">Student Name</h2>
+          </div>
+          <div className="grid-column-heading">
+            <h2 className="grid-column-heading-text">Time Entered</h2>
+          </div>
+          <div className="grid-column-heading">
+            <h2 className="grid-column-heading-text">Time Remaining</h2>
+          </div>
         </div>
+        { studentData.map((student) => {
+          return (
+            <div className="grid-container">
+              <div className="grid-column-heading">
+                <h2 className="grid-column-heading-text">{student.id}</h2>
+              </div>
+              <div className="grid-column-heading">
+                <h2 className="grid-column-heading-text">{student.name}</h2>
+              </div>
+              <div className="grid-column-heading">
+                <h2 className="grid-column-heading-text">{student.time_entered}</h2>
+              </div>
+              <div className="grid-column-heading">
+                <h2 className="grid-column-heading-text">{student.time_left}</h2>
+              </div>
+            </div>
+        )})}
       </div>
     </>
   )
