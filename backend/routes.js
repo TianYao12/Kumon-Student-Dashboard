4d608f6658fcd4ce78e039d578f0ff01b8d333f6
Hadi Youssef <hmyoussef@gmail.com>
2024-08-18 16:52:11 -0400

    Assigned roles on routes js

--- backend/routes.js
+++ backend/routes.js
@@ -5,27 +5,27 @@ router.get('/get_all_students', (req, res) => {
 
 })
 
-router.get('/get_all_current_students', (req, res) => {
+router.get('/get_all_current_students', (req, res) => { // Hetul
 
 })
 
-router.post('/add_student/:id', (req, res) => {
+router.post('/add_student/:id', (req, res) => { // Tian
 
 })
 
-router.post('/add_current_student/:id', (req, res) => {
+router.post('/add_current_student/:id', (req, res) => { // Hetul
 
 })
 
-router.put('/update_student/:id', (req, res) => {
+router.put('/update_student/:id', (req, res) => { // Tian
 
 })
 
-router.put('/update_current_student/:id', (req, res) => {
+router.put('/update_current_student/:id', (req, res) => { // Hadi
 
 })
 
-router.delete('/delete_student/:id', (req, res) => {
+router.delete('/delete_student/:id', (req, res) => { // Hadi
 
 })
 
