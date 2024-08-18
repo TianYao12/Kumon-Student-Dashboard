445be79357e0a87dc6489a5d7efd543d56d8926f
Hetul Patel <patehetu239@gmail.com>
2024-08-18 15:56:36 -0400

    initialize all routes

---
+++ backend/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/get_all_students', (req, res) => {
+
+})
+
+router.get('/get_all_current_students', (req, res) => {
+
+})
+
+router.post('/add_student/:id', (req, res) => {
+
+})
+
+router.post('/add_current_student/:id', (req, res) => {
+
+})
+
+router.put('/update_student/:id', (req, res) => {
+
+})
+
+router.put('/update_current_student/:id', (req, res) => {
+
+})
+
+router.delete('/delete_student/:id', (req, res) => {
+
+})
+
+router.delete('/delete_current_student/:id', (req, res) => {
+
+})
+
+module.exports = router;
+
+
