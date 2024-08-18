353834f00a3bd89b7430aef3033ecea859c2fafc
Hetul Patel <patehetu239@gmail.com>
2024-08-18 14:29:29 -0400

    reconfigured firebase and google auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import StudentDashbaord from "./components/StudentDashboard";
-import GoogleSignIn from "./components/GoogleSignIn"
 import { useEffect, useState } from "react";
-import { auth } from "./firebase/firebase";
-import { User } from "firebase/auth";
 import AllStudentsDashboard from "./components/AllStudentsDashboard";
 
 const App = () => {
-    const [user, setUser] = useState<User | null>(null);
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            return setUser(user);
-        })
-    })
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={user ? <Navigate to="/dashboard" /> : <GoogleSignIn />} />
                 <Route path="/dashboard" element={<StudentDashbaord />} />
-                <Route path="/signin" element={<GoogleSignIn />} />
                 <Route path="/allstudents" element={<AllStudentsDashboard />} />
             </Routes>
         </BrowserRouter>
