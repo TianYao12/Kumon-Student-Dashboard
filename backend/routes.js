7dd07fce99dfba27cd3d0285015cbf8fc64c2fea
Hetul Patel <patehetu239@gmail.com>
2024-08-18 16:55:57 -0400

    removed dynamic routing to add for routing

--- backend/routes.js
+++ backend/routes.js
@@ -6,30 +6,30 @@ router.get('/get_all_students', (req, res) => {
 })
 
 router.get('/get_all_current_students', (req, res) => { // Hetul
+    
+})
+
+router.post('/add_student/', (req, res) => { // Tian
 
 })
 
-router.post('/add_student/:id', (req, res) => { // Tian
+router.post('/add_current_student/', (req, res) => { // Hetul
 
 })
 
-router.post('/add_current_student/:id', (req, res) => { // Hetul
+router.put('/update_student/', (req, res) => { // Tian
 
 })
 
-router.put('/update_student/:id', (req, res) => { // Tian
+router.put('/update_current_student/', (req, res) => { // Hadi
 
 })
 
-router.put('/update_current_student/:id', (req, res) => { // Hadi
+router.delete('/delete_student/', (req, res) => { // Hadi
 
 })
 
-router.delete('/delete_student/:id', (req, res) => { // Hadi
-
-})
-
-router.delete('/delete_current_student/:id', (req, res) => {
+router.delete('/delete_current_student/', (req, res) => {
 
 })
 
